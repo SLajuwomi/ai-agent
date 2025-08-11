d57bf6d445d783dbb7017eaaad585c9f0e4422cd
SLajuwomi <stephenlajuwomi@gmail.com>
2025-08-11 11:25:42 -0500

    did some cleanup with the get files info code

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,7 +1,21 @@
 import os
 
 def get_files_info(working_directory, directory="."):
-    if not os.path.abspath(directory).startswith(os.path.abspath(working_directory)) and directory != working_directory:
+
+    final_string = ""
+
+    working_directory_abs_path = os.path.abspath(working_directory)
+    target_directory = os.path.abspath(os.path.join(working_directory, directory))
+
+    if not target_directory.startswith(working_directory_abs_path):
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
-    else:
-        return "good"
+    if not os.path.isdir(target_directory):
+        return f'Error: "{directory}" is not a directory'
+
+    try:
+        for filename in os.listdir(target_directory):
+            file_path = os.path.join(target_directory, filename)
+            final_string += f"- {filename}: file_size={os.path.getsize(file_path)} bytes, is_dir={os.path.isdir(file_path)} \n"            
+        return final_string
+    except Exception as e:
+        return f"An unexpected error occurred: {e}"
